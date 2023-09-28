45aa713938d16aa54c54b867aacb1c71a8e0c744
Thundox <a95tofz5@gmail.com>
2023-09-28 13:29:33 +1000

    Finished Youtube (Movement) Tutorial
    FIRST PERSON MOVEMENT in 10 MINUTES - Unity Tutorial by Dave / GameDevelopment
    https://www.youtube.com/watch?v=f473C43s8nE&

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,15 @@ public class PlayerMovement : MonoBehaviour
     [Header ("Movement")]
     public float MovementSpeed;
     public float GroundDrag;
-    public float JumpHeight;
+    //public float JumpHeight;
+
+    public float JumpForce;
+    public float JumpCooldown;
+    public float AirMultiplier;
+    public bool ReadyToJump;
+
+    [Header("Keybinds")]
+    public KeyCode jumpkey = KeyCode.Space;
 
     [Header("Ground Check")]
     public float PlayerHeight;
@@ -29,6 +37,7 @@ public class PlayerMovement : MonoBehaviour
     {
         PlayerRigidbody = GetComponent<Rigidbody>();
         PlayerRigidbody.freezeRotation = true;
+        ReadyToJump = true;
 
     }
 
@@ -39,7 +48,8 @@ public class PlayerMovement : MonoBehaviour
         Grounded = Physics.Raycast(transform.position, Vector3.down, PlayerHeight * 0.5f + 0.2f, WhatIsGround);
         
         MyInput();
-        
+        SpeedControl();
+
         // Handle Drag
         if (Grounded) 
         {
@@ -60,14 +70,50 @@ public class PlayerMovement : MonoBehaviour
     {
         HorizontalInput = Input.GetAxisRaw("Horizontal");
         VerticalInput = Input.GetAxisRaw("Vertical");
-
+        if (Input.GetKey(jumpkey) && ReadyToJump && Grounded)
+        {
+            ReadyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), JumpCooldown);
+        }
     }
     
     private void MovePlayer()
     {
         MoveDirection = OrientationTransform.forward * VerticalInput + OrientationTransform.right * HorizontalInput;
         // Replace 10f with acceleration variable
+
+        // On Ground
+        if (Grounded)
         PlayerRigidbody.AddForce(MoveDirection.normalized * MovementSpeed * 10f, ForceMode.Force);
 
+        // In Air
+        else if(!Grounded)
+        PlayerRigidbody.AddForce(MoveDirection.normalized * MovementSpeed * 10f * AirMultiplier, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(PlayerRigidbody.velocity.x, 0f, PlayerRigidbody.velocity.z);
+
+        // Limit Velocity if needed
+        if (flatVel.magnitude > MovementSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * MovementSpeed;
+            PlayerRigidbody.velocity = new Vector3(limitedVel.x, PlayerRigidbody.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        // reset y velocity
+        PlayerRigidbody.velocity = new Vector3(PlayerRigidbody.velocity.x, 0f, PlayerRigidbody.velocity.z);
+
+        PlayerRigidbody.AddForce(transform.up * JumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        ReadyToJump = true;
     }
 }
