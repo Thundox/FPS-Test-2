d0ab060cce85aea936b940c667b385268b80a5fd
Thundox <a95tofz5@gmail.com>
2023-10-26 17:12:56 +1100

    Unity tutorial finished (Throwing Projectiles)
    Completed THROWING Grenades, Knives and Other Objects - Unity Tutorial
    by Dave / GameDevelopment.
    
    - Several basic enemies setup in a firing range to test projectiles
    - Involves throwing projectiles (currently spheres)
    from player
    - Fixed bug not mentioned in tutorial involving projectiles colliding with player, Player and projectiles now have their own layers and those layers have been set not to interact with each other
    - Bug involving projectile taking on scale of object hit has been chosen to be ignored due to later objects not having incompatible scales
    (later objects will likely have a scale of 1)

---
+++ Assets/Scripts/ProjectileAddon.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileAddon : MonoBehaviour
+{
+    public int damage;
+    private Rigidbody rb;
+    private bool targetHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // make sure to only stick to the first target hit
+        if (targetHit == true)
+        {
+            return;
+        }
+        else
+            targetHit = true;
+
+        // check if you hit an enemy
+        if (collision.gameObject.GetComponent<BasicEnemy>() != null)
+        {
+            BasicEnemy enemy = collision.gameObject.GetComponent<BasicEnemy>();
+            enemy.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+
+        // make sure projectile sticks to surface
+        rb.isKinematic = true;
+
+        // make sure projectile moves with target
+        transform.SetParent(collision.transform);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
