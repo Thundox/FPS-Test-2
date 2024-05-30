3dff48b2125041b2123dfdb67401a7e9b31701ae
Thundox <a95tofz5@gmail.com>
2024-05-30 14:20:52 +1000

    Room 2 walls finished + started key script
    Room 2 Internal walls finished
    started key script, stopped before using switch statement

---
+++ Assets/Key.cs
@@ -0,0 +1,30 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public int keyNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            // update key if Player touches key
+           // other.GetComponent<KeysPlayer>().
+        }
+
+
+    }
+}
---
+++ Assets/KeysPlayer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeysPlayer : MonoBehaviour
+{
+    public bool Key1;
+    public bool Key2;
+    public bool Key3;
+    public bool Key4;
+    public bool Key5;
+    // Add switch function to assigning keys
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
