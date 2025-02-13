e4912845f0c59d164b4c47ecc37af5c43de7b16f
Thundox <a95tofz5@gmail.com>
2025-02-13 12:03:32 +1100

    Added 3 entries to score display
    added
    Kills
    Deaths
    time elapsed (not yet tracked or implemented)

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -7,13 +7,19 @@ public class ScoreDisplay : MonoBehaviour
 {
     public TextMeshProUGUI keyScoreText;
     public TextMeshProUGUI damageScoreText;
+    public TextMeshProUGUI killScoreText;
+    public TextMeshProUGUI deathScoreText;
+    public TextMeshProUGUI timeScoreText;
     public ScoreTracker scoreTracker;
     // Start is called before the first frame update
     void Start()
     {
         scoreTracker = ScoreTracker.instance;
-        keyScoreText.text = "Key Score: " + scoreTracker.keyScore;
-        damageScoreText.text = "Damage Score: " + scoreTracker.damageScore;
+        keyScoreText.text = "Keys Collected: " + scoreTracker.keyScore;
+        damageScoreText.text = "Damage dealt: " + scoreTracker.damageScore;
+        killScoreText.text = "Kills: " + scoreTracker.killScore;
+        deathScoreText.text = "Deaths: " + scoreTracker.deathScore;
+        timeScoreText.text = "time elapsed: " + scoreTracker.timeScore;
     }
 
     // Update is called once per frame
