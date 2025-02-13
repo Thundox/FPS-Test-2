e74a99777cff754f5738fae8c79eca252241fc84
Thundox <a95tofz5@gmail.com>
2025-02-13 14:02:51 +1100

    Animated Damage score increase

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -11,6 +11,7 @@ public class ScoreDisplay : MonoBehaviour
     public TextMeshProUGUI deathScoreText;
     public TextMeshProUGUI timeScoreText;
     public ScoreTracker scoreTracker;
+    public float animationDuration;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +21,29 @@ public class ScoreDisplay : MonoBehaviour
         killScoreText.text = "Kills: " + scoreTracker.killScore;
         deathScoreText.text = "Deaths: " + scoreTracker.deathScore;
         timeScoreText.text = "time elapsed: " + scoreTracker.timeScore;
+        StartCoroutine(AnimateScore());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public IEnumerator AnimateScore()
+    {
+        float elapsedTime = 0f;
+        int currentScore = 0;
+        while (elapsedTime <= animationDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float progress = elapsedTime / animationDuration;
+
+            currentScore = Mathf.FloorToInt(Mathf.Lerp(0, scoreTracker.damageScore, progress));
+            damageScoreText.text = currentScore.ToString();
+            yield return null;
+        }
+        // Makes sure it displays the correct score at end of animation
+        damageScoreText.text = scoreTracker.damageScore.ToString();
     }
 }
