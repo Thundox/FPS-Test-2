15401f683462190c3688eff3fd01851df14b97f7
Thundox <117874763+Thundox@users.noreply.github.com>
2025-10-30 01:32:19 +1100

    Grenade parts cleanup
    Grenade lever and pin destroyed after a certain amount of time has passed
    (current default 10 seconds)

--- Assets/Scripts/RemoveParent.cs
+++ Assets/Scripts/RemoveParent.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RemoveParent : MonoBehaviour
 {
+    public float grenadeCreationTime;
+    public float grenadeDespawnTime = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.parent = null;
+        grenadeCreationTime = Time.time;
+        Debug.Log("Grenade created " + grenadeCreationTime);
+    }
+    public void Update()
+    {
+        if (Time.time - grenadeCreationTime >= grenadeDespawnTime)
+        {
+            Destroy(gameObject);
+            Debug.Log("Grenade destroyed " + Time.time);
+        }
     }
 
 }
